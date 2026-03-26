92869edea568983ba7701fb7ad9682569c9c3bd4
tgbbb43 <tgbbb43@gmail.com>
2026-03-26 20:05:02 +0200

    lol4

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ print("Run by A")
 
 remove_last_task()
 add_task("B: replaced last task")
+
+def update_first_task(new_task):
+    with open("tasks.txt", "r") as f:
+        lines = f.readlines()
+    lines[0] = new_task + "\n"
+    with open("tasks.txt", "w") as f:
+        f.writelines(lines)
